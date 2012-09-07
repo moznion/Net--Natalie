5157132522945cfaf77fb3e8fa1c6e6247883686
moznion <8d49h1akimew.ume@gmail.com>
2012-09-07 16:55:22 +0900

    Implement a function of get feeds title.

--- natalie_rss.pl
+++ natalie_rss.pl
@@ -10,28 +10,33 @@ use XML::Feed;
 use Furl;
 
 my $content_name = 'comic'; #TODO This variable should be set by the constructor. This is mock-up.
+
 unless ($content_name =~ /comic|music|owarai/) {
     die "Natalie does not have such content : $content_name";
 }
-my $uri  = "http://natalie.mu/$content_name/feed/news";
-my $feed = XML::Feed->parse(URI->new($uri)) or die XML::Feed->errstr;
+my $base_uri = "http://natalie.mu/$content_name/news";
+my $rss_uri  = "http://natalie.mu/$content_name/feed/news";
+my $feed = XML::Feed->parse(URI->new($rss_uri)) or die XML::Feed->errstr;
 
-my $latest_uri;
 my $latest_serial = 0;
 foreach my $entry ($feed->entries) {
     $entry->link =~ m#/(\d*)$#;
     if ($1 > $latest_serial) {
         $latest_serial = $1;
-        $latest_uri    = $entry->link;
     }
 }
 
+my $number_of_wanna_get = 10; #TODO This variable should be set by the constructor.
 my $furl = Furl->new(
     agent => 'foo', #FIXME set correctly agent name
     timeout => 10
 );
-my $res = $furl->get($latest_uri);
-
-if ($res->content =~ m#<meta property="og:url".*/$content_name/#) {
-    say 'yes';
+for (my $iter = 0, my $ratio = 0; $iter < $number_of_wanna_get; $ratio++) {
+    my $serial_number = $latest_serial - $ratio;
+    my $res = $furl->get("$base_uri/$serial_number");
+    if ($res->content =~ m#<meta property="og:url".*/$content_name/#) {
+        $res->content =~ m#<title>(.*)</title>#;
+        print "$1\n";
+        $iter++;
+    }
 }
