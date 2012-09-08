48b9bbcea022ab2c2e2924c1fe53ae4c57263fe2
moznion <8d49h1akimew.ume@gmail.com>
2012-09-08 17:21:25 +0900

    Added pod.

--- lib/Net/Natalie.pm
+++ lib/Net/Natalie.pm
@@ -148,4 +148,31 @@ sub fetch_entry_info_manually {
 
     return $this->__parse_manually_by( 'info', $num_of_entry_to_get );
 }
+
 1;
+__END__
+
+=head1 NAME
+
+Net::Natalie -
+
+=head1 SYNOPSIS
+
+  use Net::Natalie;
+
+=head1 DESCRIPTION
+
+Net::Natalie is
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.com<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
