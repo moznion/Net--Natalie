a3db60672f6726456fcaab50bf856f7b3bb6842a
moznion <8d49h1akimew.ume@gmail.com>
2012-09-08 23:27:44 +0900

    Improved sample.pl

--- eg/sample.pl
+++ eg/sample.pl
@@ -8,8 +8,14 @@ use lib ("$FindBin::Bin/../lib");
 
 use Net::Natalie;
 
-my $foo = Net::Natalie->new( content => 'comic', get_num => 2 );
-my %bar = $foo->fetch_entry_title_by_feed;
+my $foo = Net::Natalie->new( content => 'comic' );
+
+my %bar = $foo->fetch_entry_info_manually(2); #Get 2 entries by http query
 foreach ( keys %bar ) {
-    print "$bar{$_}: $_\n";
+    print "$bar{$_}->[0]: $bar{$_}->[1] -- $_\n"; #<title of entry>: <summary of entry> -- <URL>
+}
+
+my %baz = $foo->fetch_entry_info_by_feed; #Get entries by rss feed. (Maybe, it takes 50 entries)
+foreach ( keys %baz ) {
+    print "$baz{$_}->[0]: $baz{$_}->[1] -- $_\n"; #<title of entry>: <summary of entry> -- <URL>
 }
